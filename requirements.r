4afa6b8f080c69a954b6f37ca400169a66cf4de6
Filip Schouwenaars <filip@datacamp.com>
2018-03-27 19:09:49 +0200

    bump reqs

--- requirements.r
+++ requirements.r
@@ -1,4 +1,4 @@
-devtools::install_version("tibble", "1.2")
+devtools::install_version("tibble", "1.4.2")
 devtools::install_version("plyr", "1.8.4")
-devtools::install_version("dplyr", "0.5.0")
-devtools::install_version("tidyr", "0.6.0")
+devtools::install_version("dplyr", "0.7.4")
+devtools::install_version("tidyr", "0.8.0")
